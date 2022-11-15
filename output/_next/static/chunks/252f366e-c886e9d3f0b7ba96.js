b28f2e9058e1ff945c048a4ff5ef8184888b06f2
Samson Sanyaolu <sosarenaint@gmail.com>
2022-11-16 01:29:45 +0400

    output

---
+++ output/_next/static/chunks/252f366e-c886e9d3f0b7ba96.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{5434:function(t,n,a){a.d(n,{KEG:function(){return r}});var e=a(8357);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"}},{tag:"path",attr:{d:"M6.23 20.23L8 22l10-10L8 2 6.23 3.77 14.46 12z"}}]})(t)}}}]);
